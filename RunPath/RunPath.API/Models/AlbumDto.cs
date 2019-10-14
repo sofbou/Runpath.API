b1045eeb5d3760252602c6adf2632da176685f47
sofbou <34511745+sofbou@users.noreply.github.com>
2019-10-14 22:41:32 +0100

    Update AlbumDto.cs

--- RunPath/RunPath.API/Models/AlbumDto.cs
+++ RunPath/RunPath.API/Models/AlbumDto.cs
@@ -10,7 +10,6 @@ namespace RunPath.API.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public int UserId { get; set; }
-        public ICollection<PhotoDto> Photos { get; set; } = new List<PhotoDto>();
     }
 }
 
